321323fd8b223bdd1f3bdd24066d1025cf72afe8
Timo Mihaljov <timo.mihaljov@solita.fi>
2015-12-14 09:46:15 +0200

    Move support code to TestHelper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,7 +1,14 @@
 require 'open3'
+gem 'minitest'
+require 'minitest/autorun'
 require 'minitest/focus'
+require 'mechanize'
 
 module TestHelper
+
+  def setup
+    @agent = Mechanize.new { |a| a.log = Logger.new('/tmp/mechanize.log') }
+  end
 
   def unindent(s)
     indent = s[/[ \t]*/]
@@ -24,6 +31,39 @@ module TestHelper
     ensure
       p.unlink
     end
+  end
+
+  def open_dashboard
+    @agent.get('http://localhost:8080')
+  end
+
+  def can_manage_jenkins?
+    not @agent.page.link_with(:text => 'Manage Jenkins').nil?
+  end
+
+  def assert_forbidden(&block)
+    begin
+      yield
+      flunk 'Expected 403, got 200/301/302'
+    rescue Mechanize::ResponseCodeError => e
+      assert_equal "403", e.response_code
+    end
+  end
+
+  def login_as(username)
+    page = @agent.get('http://localhost:8080/login')
+    form = page.form('login')
+    form.j_username = username
+    form.j_password = File.read("environments/vagrant/solita_jenkins_default_password/#{username}").strip
+    @agent.submit(form, form.buttons.first)
+  end
+
+  def list_users
+    @agent.get('http://localhost:8080/securityRealm')\
+      .search('//table[@id="people"]//tr/td[2]//*')\
+      .map { |n| n.text }\
+      .to_set\
+      .delete('solita_jenkins')
   end
 
 end
--- test/test_security.rb
+++ test/test_security.rb
@@ -1,15 +1,8 @@
-gem 'minitest'
-require 'minitest/autorun'
-require 'mechanize'
 require 'test_helper'
 
 class TestSecurity < Minitest::Test
 
   include TestHelper
-
-  def setup
-    @agent = Mechanize.new { |a| a.log = Logger.new('/tmp/mechanize.log') }
-  end
 
   # When solita_jenkins_security_realm is set to "none", Jenkins is unsecured.
   def test_realm_none
@@ -146,39 +139,6 @@ class TestSecurity < Minitest::Test
     # removed.
     login_as 'solita_jenkins'
     assert_equal ['xyz'].to_set, list_users
-  end
-
-  def open_dashboard
-    @agent.get('http://localhost:8080')
-  end
-
-  def can_manage_jenkins?
-    not @agent.page.link_with(:text => 'Manage Jenkins').nil?
-  end
-
-  def assert_forbidden(&block)
-    begin
-      yield
-      flunk 'Expected 403, got 200/301/302'
-    rescue Mechanize::ResponseCodeError => e
-      assert_equal "403", e.response_code
-    end
-  end
-
-  def login_as(username)
-    page = @agent.get('http://localhost:8080/login')
-    form = page.form('login')
-    form.j_username = username
-    form.j_password = File.read("environments/vagrant/solita_jenkins_default_password/#{username}").strip
-    @agent.submit(form, form.buttons.first)
-  end
-
-  def list_users
-    @agent.get('http://localhost:8080/securityRealm')\
-      .search('//table[@id="people"]//tr/td[2]//*')\
-      .map { |n| n.text }\
-      .to_set\
-      .delete('solita_jenkins')
   end
 
 end
